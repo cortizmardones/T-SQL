97db65bda6784e3147a982f887aaa85bbf5d5711
Carlos Ortiz <cortizmardones@gmail.com>
2019-07-23 10:59:40 -0400

    fix script poblamiento

--- Poblamiento BANCOS.sql
+++ Poblamiento BANCOS.sql
@@ -154,22 +154,19 @@ insert into dbo.Pueblo_Originario values ('Cunco',1,500);
 insert into dbo.Pueblo_Originario values ('Chono',1,500);
 
 
-select * from Certificado_permanencia; --Cree la tabla con identy
+go --select * from Certificado_permanencia; --Cree la tabla con identy
 
 insert into Certificado_permanencia values ('Sin certificado',0);
 insert into Certificado_permanencia values ('Con certificado',1);
 
-
-select * from relacion_carga;
+go
 
 insert into relacion_carga values (1,'Conyuge');
 insert into relacion_carga values (2,'Hijo - Hija');
 insert into relacion_carga values (3,'Nieto - Nieta');
 insert into relacion_carga values (4,'Otro');
 
-
-select * from Conyuge;
-select * from Nacionalidad;
+go
 
 insert into dbo.Conyuge values (171487965,'Camila','Sanchez','Somoza','19880322',1);
 insert into dbo.Conyuge values (139943317,'Claudio','Angulo','Briones','19600718',1);
@@ -178,7 +175,7 @@ insert into dbo.Conyuge values (105432881,'Carla','Mardones','Quilodran','195505
 insert into dbo.Conyuge values (208334721,'Venancio','Castro','Soto','20001001',1);
 
 
-select * from Carga;
+go
 
 insert into Carga values (240224932,'Esteban','Paredes','Gonzales','20190322');
 insert into Carga values (265803175,'Ricardo','Milos','Ugarte','20190512');
@@ -186,8 +183,7 @@ insert into Carga values (206683183,'Maria','Estanford','Maluenda','20150712');
 insert into Carga values (208956723,'Isabel','Mardones','Picaporte','20150902');
 insert into Carga values (192239378,'Diego','Llanquin','Astroza','20131101');
 
-
-select * from Consultor;
+go
 
 insert into dbo.Consultor values (105395949,'Emilio','Sanhueza','esanhueza@minvu.cl',958586705);
 insert into dbo.Consultor values (206595302,'Estefanie','Milan','emilan@minvu.cl',93578143);
@@ -195,18 +191,15 @@ insert into dbo.Consultor values (148956723,'Nevenka','Malloa','nmalloa@minvu.cl
 insert into dbo.Consultor values (210276580,'Raul','Pardo','rpardo@minvu.cl',930246705);
 insert into dbo.Consultor values (55461066,'Jose','Collio','jcollio@minvu.cl',915667188);
 
+go
 
-select * from Cuenta;
-select * from Banco;
-select * from dbo.Tipo_Cuenta;
+insert into cuenta values (667788,3,1,8500000,'2019-07-23','2019-07-23');
+insert into cuenta values (327195,3,10,9275833,'2019-07-23','2019-07-23');
+insert into cuenta values (167284,3,6,17500423,'2019-07-23','2019-07-23');
+insert into cuenta values (102871,3,8,12799000,'2019-07-23','2019-07-23');
+insert into cuenta values (897726,3,4,233000,'2019-07-23','2019-07-23');
 
-insert into cuenta values (221476,'AHORRO VIVIENDA',3,1,8500000,null,null);
-insert into cuenta values (327195,'AHORRO VIVIENDA',3,10,9275833,null,null);
-insert into cuenta values (167284,'AHORRO VIVIENDA',3,6,17500423,null,null);
-insert into cuenta values (102871,'AHORRO VIVIENDA',3,8,12799000,null,null);
-insert into cuenta values (897726,'AHORRO VIVIENDA',3,4,233000,null,null);
-
-select * from dbo.Deposito;
+go
 
 insert into dbo.Deposito values (1,8500000);
 insert into dbo.Deposito values (2,9275833);
@@ -214,7 +207,7 @@ insert into dbo.Deposito values (3,17500423);
 insert into dbo.Deposito values (4,12799000);
 insert into dbo.Deposito values (5,233000);
 
-select * from dbo.Deposito_Cuenta;
+go
 
 insert into dbo.Deposito_Cuenta values (1,221476,1,'20180801');
 insert into dbo.Deposito_Cuenta values (2,327195,2,'20181019');
@@ -222,27 +215,20 @@ insert into dbo.Deposito_Cuenta values (3,167284,3,'20170315');
 insert into dbo.Deposito_Cuenta values (4,102871,4,'20161224');
 insert into dbo.Deposito_Cuenta values (5,897726,5,'20190113');
 
+go
 
-select * from Domicilio;
-select * from Comuna;
+insert into dbo.Domicilio values (1,'El concretero','01798','','','','',1,'19800501',1,'San Jose de la Construcción','Cordillera','2019-07-23','2019-07-23');
+insert into dbo.Domicilio values (2,'Avenida Ejercito Libertador','215','C','33','','',1,'19720422',1,'Los Alerces','Santiago Centro','2019-07-23','2019-07-23');
+insert into dbo.Domicilio values (3,'Bron Sur','2520','','77B','','',1,'20141117',51,'Santa Julia','Frente al consultorio','2019-07-23','2019-07-23');
+insert into dbo.Domicilio values (4,'Callejon Capturi','77193','','','','',1,'19720422',9,'Peninsula de fuego','A un costado del estadio municipal','2019-07-23','2019-07-23');
+insert into dbo.Domicilio values (5,'Travesia Baratem','01644','','','','',1,'19720422',40,'Catemito Norte','Cerca del terminal rodoviario','2019-07-23','2019-07-23');
 
-insert into dbo.Domicilio values (1,'El concretero','01798','','','','',1,'19800501',1,'San Jose de la Construcción','Cordillera',null,null);
-insert into dbo.Domicilio values (2,'Avenida Ejercito Libertador','215','C','33','','',1,'19720422',1,'Los Alerces','Santiago Centro',null,null);
-insert into dbo.Domicilio values (3,'Bron Sur','2520','','77B','','',1,'20141117',51,'Santa Julia','Frente al consultorio',null,null);
-insert into dbo.Domicilio values (4,'Callejon Capturi','77193','','','','',1,'19720422',9,'Peninsula de fuego','A un costado del estadio municipal',null,null);
-insert into dbo.Domicilio values (5,'Travesia Baratem','01644','','','','',1,'19720422',40,'Catemito Norte','Cerca del terminal rodoviario',null,null);
-
-
-select * from Tipo_vivienda;
+go
 
 insert into Tipo_vivienda values (1,'Casa');
 insert into Tipo_vivienda values (2,'Departamento');
 
-select * from Vivienda;
-select * from Tramo_vivienda;
-select * from Domicilio;
-select * from Propietario;
-select * from Comuna;
+go
 
 insert into Vivienda values (1,'Casa Viena',2600,1, 3,163002957,1);
 insert into Vivienda values (2,'Casa Londres',2000,1,2,169199957,2);
@@ -250,11 +236,7 @@ insert into Vivienda values (3,'Casa Niza',1450,1,1,178569465,3);
 insert into Vivienda values (4,'Departamento Sevilla',2799,2,3,184997142,4);
 insert into Vivienda values (5,'Casa Milán',2100,1,2,219684886,5);
 
-
-
-
-select * from Postulante_Conyuge;
-select * from Postulante;
+go
 
 insert into Postulante_Conyuge values (94737419,105432881,'20190401',1,1);
 insert into Postulante_Conyuge values (155106514,117236125,'20190415',1,2);
@@ -262,11 +244,7 @@ insert into Postulante_Conyuge values (167746322,139943317,'20190522',1,3);
 insert into Postulante_Conyuge values (171302838,171487965,'20190413',1,4);
 insert into Postulante_Conyuge values (222872707,208334721,'20190501',1,5);
 
-select * from carga;
-select * from Postulante;
-select * from relacion_carga;
-
-select * from Postulante_Carga;
+go
 
 insert into Postulante_Carga values (167746322,192239378,'20190401',1,1);
 insert into Postulante_Carga values (155106514,206683183,'20190415',1,2);
@@ -274,8 +252,7 @@ insert into Postulante_Carga values (171302838,208956723,'20190522',1,3);
 insert into Postulante_Carga values (94737419,240224932,'20190413',1,4);
 insert into Postulante_Carga values (222872707,265803175,'20190501',1,1);
 
-
-select * from dbo.informe_tecnico;
+go
 
 insert into dbo.informe_tecnico values (8081,'20190401','20190411',8081,1,55461066);
 insert into dbo.informe_tecnico values (8082,'20190415','20190425',8082,2,105395949);
@@ -283,7 +260,7 @@ insert into dbo.informe_tecnico values (8083,'20190522','20190602',8083,3,148956
 insert into dbo.informe_tecnico values (8084,'20190413','20190423',8084,4,206595302);
 insert into dbo.informe_tecnico values (8085,'20190501','20190511',8085,5,210276580);
 
-select * from Formulario_A;
+go
 
 insert into Formulario_A values (8081,'20190401','Ernesto Perez',167746322,1);
 insert into Formulario_A values (8082,'20190415','Ernesto Perez',155106514,2);
@@ -291,7 +268,7 @@ insert into Formulario_A values (8083,'20190522','Fabiola Tapia',171302838,3);
 insert into Formulario_A values (8084,'20190413','Ernesto Perez',94737419,4);
 insert into Formulario_A values (8085,'20190501','Ernesto Perez',222872707,5);
 
-select * from Postulante;
+go
 
 insert into Postulante values (167746322,'Jose','Miguel','Viñuela','Moreno',2557036,91234567,91234567,0140000,'walter.white@gmail.com','19850701',1,2,102871,1,1350000,8,2,1);
 insert into Postulante values (155106514,'Romina','Alejandra','Jara','Romero',2557127,92569871,920897451,8320000,'romina.jara@gmail.com','19911121',2,5,167284,2,1750000,2,5,2);
@@ -299,28 +276,21 @@ insert into Postulante values (171302838,'Francisco','Orlando','Bernales','Ortiz
 insert into Postulante values (94737419,'Nicole','Andrea','Mardones','Quilodran',26567763,982547612,921456172,2340000,'nicole.mardones@gmail.com','19911121',2,3,327195,4,1999000,2,4,1);
 insert into Postulante values (222872707,'Claudio','Andres','Sandoval','Matus',26567766,963415782,993628876,4030000,'claudio.sandoval@gmail.com','19850701',1,1,897726,5,1192540,4,6,1);
 
---Me faltan las tablas:  (PROCESO - Se llena con un procedimiento de almacenado)
-
-
-
-
-
-
 
 --Mantenedores de puntajes
-select * from Tramo_Edad;
+go
 
 insert into Tramo_Edad values (1,18,29,200);
 insert into Tramo_Edad values (2,30,39,150);
 insert into Tramo_Edad values (3,40,50,100);
 
-select * from Tramo_Carga_Familiar;
+go
 
 insert into Tramo_Carga_Familiar values (1,5,11,200);
 insert into Tramo_Carga_Familiar values (2,2,4,100);
 insert into Tramo_Carga_Familiar values (3,0,1,50);
 
-select * from Estado_Civil; --(Ya cargado los puntajes en la tabla)
+go
 
 insert into dbo.[Estado_Civil] values (1,'Soltero',60);
 insert into dbo.[Estado_Civil] values (2,'Casado',100);
@@ -328,15 +298,13 @@ insert into dbo.[Estado_Civil] values (3,'Viudo',20);
 insert into dbo.[Estado_Civil] values (4,'Conviviente Civil',80);
 insert into dbo.[Estado_Civil] values (5,'Divorciado',40);
 
-
---Para calcular puntajes.
-select * from tipo_titulo;
+go
 
 insert into tipo_titulo values (1,'Título Profesional',200);
 insert into tipo_titulo values (2,'Título Técnico',100);
 insert into tipo_titulo values (3,'Sin titulo',0);
 
-select * from Titulo;
+go
 
 insert into Titulo values (1,'Sin Titulo',0,3);
 insert into Titulo values (2,'Ingeniero en Informatica',1,1);
@@ -345,15 +313,13 @@ insert into Titulo values (4,'Arquitecto de Software',1,1);
 insert into Titulo values (5,'Asistente de soporte',1,2);
 insert into Titulo values (6,'Ingeniero en redes y telecomunicaciones',1,1);
 
-
-select * from Tramo_Ahorro;
+go
 
 insert into Tramo_Ahorro values(1,10000000,99000000,500);
 insert into Tramo_Ahorro values(2,9000000,9999999,400);
 insert into Tramo_Ahorro values(3,8000000,8999999,200);
 
-
-select * from error;
+go
 
 insert into error values(1,'El Rut ingresado ya existe en la BDD');
 insert into error values(2,'La cuenta bancaria ya existe en la BDD');
